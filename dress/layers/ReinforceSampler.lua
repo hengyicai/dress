c55824919209b25ce94633b3ffee0526ae769745
caihengyi <caihengyi@ict.ac.cn>
2018-11-12 10:59:28 +0800

    update

--- dress/layers/ReinforceSampler.lua
+++ dress/layers/ReinforceSampler.lua
@@ -13,15 +13,15 @@
 --
 
 local ReinforceSampler, parent = torch.class('nn.ReinforceSampler',
-                                              'nn.Module')
+    'nn.Module')
 -- Module that takes a tensor storing log-probabilities (output of a LogSoftmax)
 -- and samples from the corresponding multinomial distribtion.
 -- Assumption: this receives input from a LogSoftMax and receives gradients from
 -- a ReinforceCriterion.
 function ReinforceSampler:__init(distribution)
-   parent.__init(self)
-   self.distribution = distribution
-   self.prob = torch.Tensor()
+    parent.__init(self)
+    self.distribution = distribution
+    self.prob = torch.Tensor()
 end
 
 function ReinforceSampler:updateOutput(input)
@@ -30,31 +30,32 @@ function ReinforceSampler:updateOutput(input)
         self.prob:copy(input)
         self.prob:exp()
         self.output:resize(input:size(1), 1)
+        self.output = torch.LongTensor():resize(self.output:size()):copy(self.output):cuda()
         torch.multinomial(self.output, self.prob, 1)
     else
         error('we did not implement sampling from', self.distribution)
     end
-   return self.output -- batch x 1
+    return self.output -- batch x 1
 end
 
 -- NOTE: in order for this to work, it has to be connected
 -- to a ReinforceCriterion.
 function ReinforceSampler:updateGradInput(input, gradOutput)
-   if self.distribution == 'multinomial' then
-      -- loop over mini-batches and build sparse vector of gradients
-      -- such that each sample has a vector of gradients that is all 0s
-      -- except for the component corresponding to the chosen word.
-      -- We assume that the gradients are provided by a ReinforceCriterion.
-      self.gradInput:resizeAs(input)
-      self.gradInput:zero()
-      for ss = 1, self.gradInput:size(1) do
-          -- adding round because sometimes multinomial returns a float 1e-6 far
-          -- from an integer.
-          self.gradInput[ss][torch.round(self.output[ss][1])] =
-              gradOutput[ss][1]
-      end
-      return self.gradInput
-   else
-      error('we did not implement sampling from', self.distribution)
-   end
+    if self.distribution == 'multinomial' then
+        -- loop over mini-batches and build sparse vector of gradients
+        -- such that each sample has a vector of gradients that is all 0s
+        -- except for the component corresponding to the chosen word.
+        -- We assume that the gradients are provided by a ReinforceCriterion.
+        self.gradInput:resizeAs(input)
+        self.gradInput:zero()
+        for ss = 1, self.gradInput:size(1) do
+            -- adding round because sometimes multinomial returns a float 1e-6 far
+            -- from an integer.
+            self.gradInput[ss][torch.round(self.output[ss][1])] =
+            gradOutput[ss][1]
+        end
+        return self.gradInput
+    else
+        error('we did not implement sampling from', self.distribution)
+    end
 end
